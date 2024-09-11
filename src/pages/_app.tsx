279c09161be1745eb76598ffe03f8221e3ffd878
Florian Mealing <florianmealing@gmail.com>
2024-09-11 14:43:16 +0100

    Set up basic project structure with homepage and global styles

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+// src/pages/_app.tsx
+import "../styles/globals.scss";
+import { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+// src/pages/index.tsx
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to SwiftCart</h1>
+      <p>Your e-commerce platform is up and running!</p>
+    </div>
+  );
+};
+
+export default HomePage;
