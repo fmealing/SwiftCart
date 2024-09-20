196b9fe7933d28d9cddb590afbfff32b1aaa13e1
Florian Mealing <florianmealing@gmail.com>
2024-09-20 17:32:38 +0100

    Fixed Something

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,3 @@
-// pages/_document.tsx
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -7,14 +6,6 @@ export default function Document() {
       <Head>
         {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
-
-        {/* Global Meta Tags */}
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Minimalist Tech E-Commerce." />
-        <meta name="author" content="Florian Mealing" />
-        <meta property="og:title" content="Swift Cart" />
-        <meta property="og:description" content="Minimalist Tech E-commerce." />
       </Head>
       <body>
         <Main />
