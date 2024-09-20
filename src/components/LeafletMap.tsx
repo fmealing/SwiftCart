fc00ab2f81991cef4e9d445d1a939735c2bf41a1
Florian Mealing <florianmealing@gmail.com>
2024-09-20 20:06:46 +0100

    fixed the map again

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -1,10 +1,24 @@
-"use client"; // Ensure this file is client-side only
+"use client";
 
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { LatLngExpression, LatLngTuple } from "leaflet";
+import { LatLngExpression, LatLngTuple, Icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
-import "leaflet-defaulticon-compatibility";
+
+// Importing the marker images
+import iconUrl from "leaflet/dist/images/marker-icon.png";
+import iconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
+import shadowUrl from "leaflet/dist/images/marker-shadow.png";
+
+// Fix Leaflet's default icon paths
+const defaultIcon = new Icon({
+  iconUrl: iconUrl.src, // Access the src property of the image
+  iconRetinaUrl: iconRetinaUrl.src, // Access the src property for retina image
+  shadowUrl: shadowUrl.src, // Access the src property for shadow
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
 
 interface MapProps {
   posix: LatLngExpression | LatLngTuple;
@@ -27,7 +41,7 @@ const LeafletMap = ({ posix, zoom = defaults.zoom }: MapProps) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={posix}>
+      <Marker position={posix} icon={defaultIcon}>
         <Popup>You are here</Popup>
       </Marker>
     </MapContainer>
